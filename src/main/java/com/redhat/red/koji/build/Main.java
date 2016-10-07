753e4f91ad344b4417d5472fd49c5045cc40ed66
John Casey <jdcasey@commonjava.org>
2016-10-07 18:18:11 -0500

    Rename package and add license headers

--- src/main/java/com/redhat/red/koji/build/Main.java
+++ src/main/java/com/redhat/red/koji/build/Main.java
@@ -1,4 +1,19 @@
-package com.redhat.rcm.koji.build;
+/**
+ * Copyright (C) 2016 Red Hat, Inc. (jcasey@redhat.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.redhat.red.koji.build;
 
 import com.redhat.red.build.koji.KojiClient;
 import com.redhat.red.build.koji.KojiClientException;
@@ -197,11 +212,19 @@ public class Main
     void report()
             throws IOException
     {
-        reportFile = new File( "buildfinder.out.txt" );
-        try (PrintWriter pw = new PrintWriter( new FileWriter( reportFile ) ))
+        if ( !allMissing.isEmpty() )
         {
-            allMissing.forEach( ( file, missing ) -> pw.write(
-                    String.format( "%s:\n%s\n  %s\n\n", file, LINE, join( missing, "\n  " ) ) ) );
+            reportFile = new File( "buildfinder.out.txt" );
+            try (PrintWriter pw = new PrintWriter( new FileWriter( reportFile ) ))
+            {
+                allMissing.forEach( ( file, missing ) -> pw.write(
+                        String.format( "%s:\n%s\n  %s\n\n", file, LINE, join( missing, "\n  " ) ) ) );
+            }
+        }
+        else
+        {
+            Logger logger = LoggerFactory.getLogger( getClass() );
+            logger.info( "Nothing to report! All files have a corresponding Koji build!" );
         }
     }
 
